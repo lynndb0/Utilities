b1893027218d2b403d54c88f4c1e831f73d06840
David Lynn <daveblynn@gmail.com>
2026-06-24 10:19:09 +1000

    Add encode/decode url functions

--- utilities.py
+++ utilities.py
@@ -1,4 +1,4 @@
-
+import urllib.parse
 
 def remove_newlines(input_query):
   """Remove newlines from string for json formatting etc.
@@ -6,3 +6,17 @@ def remove_newlines(input_query):
 
     one_line_query = " ".join(line.strip() for line in input_query.splitlines())
     return one_line_query
+
+def encode_url(url):
+  """Encode url
+  """
+
+return urllib.parse.quote(url)
+
+return u
+
+def decode_url(url):
+  """Decode url  
+  """
+
+return urllib.parse.unquote(url)
